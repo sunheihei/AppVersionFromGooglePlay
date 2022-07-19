578f08c1c338113f830c69b1ef94738450e7f2e6
SunYiH <sunyihuan0@gmail.com>
2022-07-19 14:07:46 +0800

    first commit

---
+++ app/src/main/java/com/example/appversiongoogleplay/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.appversiongoogleplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.appversionlib.AppVersionOnGooglePlayUtils
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+//        val packName = "com.zhiliaoapp.musically"
+        val packName = "com.dasda.dasdas"
+
+        AppVersionOnGooglePlayUtils(this, packName).getVersionOnGooglePlay {
+            Log.d(TAG, "version is $it")
+        }
+
+    }
+}
---
+++ appversionlib/src/main/java/com/example/appversionlib/AppVersionOnGooglePlayUtils.kt
@@ -0,0 +1,90 @@
+package com.example.appversionlib
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.util.Log
+import okhttp3.*
+
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import java.util.regex.Pattern
+
+class AppVersionOnGooglePlayUtils(
+    private val context: Context,
+    private val packName: String,
+    private val deflateErrorVersion: String? = "0",
+
+    ) {
+
+
+    fun getVersionOnGooglePlay(versionCallback: (version: String) -> Unit) {
+
+        val url =
+            "https://play.google.com/store/apps/details?id=$packName&hl=en"
+
+
+        val client = OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build()
+        val request =
+            Request.Builder().url(url).build()
+
+        val call = client.newCall(request)
+        call.enqueue(
+            object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    versionCallback.invoke(deflateErrorVersion!!)
+                }
+
+                @Throws(IOException::class)
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val str = response.body!!.string()
+                        val version = parseVersion(str)
+//                        Log.d("MainActivity", "version： $version")
+                        if (version == deflateErrorVersion) {
+                            versionCallback.invoke(deflateErrorVersion!!)
+                        } else {
+                            versionCallback.invoke(version!!)
+                        }
+
+                    }else{
+                        versionCallback.invoke(deflateErrorVersion!!)
+                    }
+                }
+            })
+    }
+
+    private fun parseVersion(string: String): String? {
+        val pattern1 = "\\[\\[\\[\\\"[0-9]+\\.[0-9]+\\.[0-9]\\\"\\]\\]"
+        val pattern2 = "[0-9]+\\.[0-9]+\\.[0-9]"
+        val r1 = Pattern.compile(pattern1)
+        val m1 = r1.matcher(string)
+        if (m1.find()) {
+            val str2 = m1.group(0)
+            val r2 = Pattern.compile(pattern2)
+            val m2 = r2.matcher(str2)
+            if (m2.find()) {
+                return m2.group(0)
+            }
+        }
+        return deflateErrorVersion
+    }
+
+
+//    private fun googlePlayVersionCompare() {
+//        if (versionOnline == "0")
+//            return
+//        val newVOnline = versionOnline.replace(".", "").toInt()
+//        val newVLocal = getLocalVersionName().replace(".", "").toInt()
+////        Log.d("VERSION233", "newVOnline : $newVOnline")
+////        Log.d("VERSION233", "newVLocal : ${newVLocal}")
+//    }
+
+    fun getLocalVersionName(): String {
+        val packageInfo: PackageInfo = context.packageManager
+            .getPackageInfo(context.packageName, 0)
+        return packageInfo.versionName
+
+//        return BuildConfig.VERSION_NAME
+    }
+
+}
